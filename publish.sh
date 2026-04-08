be10265b3a59441a1f22fab02747e14b07788f1e
Eilene <eileneftf@gmail.com>
2026-04-08 03:20:13 -0400

    site rebuild

--- publish.sh
+++ publish.sh
@@ -2,4 +2,5 @@
 cobalt build
 git commit -a -m "site rebuild"
 git push origin main
-git subtree push --force --prefix _site origin gh-pages
+git subtree merge --prefix _site gh-pages
+git subtree push --prefix _site origin gh-pages
